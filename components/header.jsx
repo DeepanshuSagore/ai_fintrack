1eac5acbf93605ac69bfee9d9989072ec75515ae
Trash <deepanshusagore220128@acropolis.in>
2026-02-19 20:13:48 +0530

    Probably backend connection

--- components/header.jsx
+++ components/header.jsx
@@ -1,17 +1,17 @@
-"use client";
-
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
-const Header = () => {
+import { checkUser } from "@/lib/checkUser";
+const Header = async () => {
+  await checkUser();
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href= "/">
           <Image
-          src={"/logo.png"} alt="fintrack logo"
+          src={"/logo1.jpeg"} alt="fintrack logo"
           height={60} width={200}
           className="h-12 w-auto object-contain"
           />
