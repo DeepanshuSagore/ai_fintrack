1ffe79e01bc6e8b6a48245252c5e35ab0218df49
Trash <deepanshusagore220128@acropolis.in>
2026-02-24 20:20:52 +0530

    feat: enhance transaction processing and budget alert functionalities

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,19 @@
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlert } from "@/lib/inngest/functions";
+import {
+  checkBudgetAlerts,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+  generateMonthlyReports,
+} from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    checkBudgetAlert, // <-- This is where you'll always add all your functions
+    checkBudgetAlerts,
+    processRecurringTransaction,
+    triggerRecurringTransactions,
+    generateMonthlyReports,
   ],
 });
